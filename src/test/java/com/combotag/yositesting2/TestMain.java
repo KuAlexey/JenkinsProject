58cc14140ada8e2c1da4028e08fd78fa1c58b6f1
akucherenko <mailkeeper2k@gmail.com>
2018-09-27 10:59:44 +0300

    1.3

--- src/test/java/com/combotag/yositesting2/TestMain.java
+++ src/test/java/com/combotag/yositesting2/TestMain.java
@@ -8,6 +8,7 @@ public class TestMain extends FunctionalTest{
 
         @Test
         public void mainTest() {
+			/*
             LoginPage loginPage = new LoginPage();
 
             loginPage.setTextToInput("Username","QATestLab");
@@ -22,6 +23,8 @@ public class TestMain extends FunctionalTest{
             Assert.assertTrue(homePage.appearanceOfElementInTheWidget("Layout1537233134"));
             homePage.clickChevronOfElement("Layout1537233134");
             Assert.assertTrue(homePage.appearanceOfElementInTheWidget("Banner1537233134"));
+			*/
+			System.out.println(getDriver());
         }
     }
 }
