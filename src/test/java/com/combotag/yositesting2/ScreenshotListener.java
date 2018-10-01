6e421e87410776d49f2f1bb4cbae2b6b748a64fe
akucherenko <mailkeeper2k@gmail.com>
2018-10-01 16:53:15 +0300

    adding allure screenshot

---
+++ src/test/java/com/combotag/yositesting2/ScreenshotListener.java
@@ -0,0 +1,32 @@
+package com.combotag.yositesting2;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+public class ScreenshotListener extends TestListenerAdapter {
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        saveScreenshot();
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        saveScreenshot();
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        saveScreenshot();
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    private byte[] saveScreenshot() {
+        WebDriver driver = BaseTest.getDriver();
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+}
